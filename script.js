0e0860fc9bd2a3ce64d9691bd919d16b80858a48
tasfiq512 <tasfiq512@gmail.com>
2024-02-10 02:59:56 +0600

    function add task

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const inputBox = document.querySelector('.input-box')
 const listContainer = document.querySelector('.list-container')
+//function to add tasks to task list
+function addTask(){
+	if(inputBox.value === ''){
+		alert('You must write something!');
+	}
+	else{
+		let li = document.createElement('li')
+		li.innerHTML = inputBox.value;
+		listContainer.appendChild(li);
 
+		let span = document.createElement('span');
+		span.innerHTML = '\u00d7';
+		li.appendChild(span);
+	}
+	inputBox.value = '';
+	saveData();
+}
